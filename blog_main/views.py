be045259c6e86a8e53160273ecc49535142d29b2
Muskan Tarafder <muskantarafder357@gmail.com>
2026-02-24 20:32:39 +0530

    registration

--- blog_main/views.py
+++ blog_main/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from blogs.models import *
+from .forms import *
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib import auth
+
 def home(request):
     # categories = Category.objects.all()
     # print(categories)
@@ -16,3 +20,42 @@ def home(request):
     }
     return render(request,'home.html',context)
     return HttpResponse('<h2>Home Page<h2>')
+
+def register(request):
+    if request.method =='POST':
+        form= RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('register.html')
+        else:
+            print(form.errors)
+        pass
+    else:
+        form = RegistrationForm()
+    context ={
+        'form':form,
+    }
+    return render(request,'register.html',context)
+
+def login(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request,request.POST)
+        if form.is_valid():
+            username=form.cleaned_data['username']
+            password=form.cleaned_data['password']
+
+            user = auth.authenticate(username=username,password=password)
+            if user is not None:
+                auth.login(request,user)
+            return redirect('home')
+    form=AuthenticationForm()
+    context = {
+        'form':form
+    }
+    return render(request,'login.html',context)
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('home')
+
